a6a53d7a37aa8b847b3caf78988be523ba7204cc
Paul-Michael Sorhaindo <Mikey@nemo.home>
2015-01-18 10:54:41 +0000

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Paul-Michael Sorhaindo on 18/01/2015.
+//  Copyright (c) 2015 Paul-Michael Sorhaindo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
