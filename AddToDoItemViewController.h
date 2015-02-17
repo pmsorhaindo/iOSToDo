0fd597c8ee26f25d99f22ff71ff0e06193f13944
Mikey <pmsorhaindo@gmail.com>
2015-02-17 21:30:30 +0000

    checkmarks and adding todos

--- AddToDoItemViewController.h
+++ AddToDoItemViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TodoItem.h"
 
 @interface AddToDoItemViewController : UIViewController
+
+@property TodoItem *toDoItem;
 
 @end
--- ToDoListTableViewController.m
+++ ToDoListTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "ToDoListTableViewController.h"
 #import "TodoItem.h"
+#import "AddToDoItemViewController.h"
 
 @interface ToDoListTableViewController ()
 
@@ -30,7 +31,13 @@
 }
 
 - (IBAction)unwindToList:(UIStoryboardSegue *)segue {
+    AddToDoItemViewController *source = [segue sourceViewController];
+    TodoItem *item = source.toDoItem;
     
+    if (item != nil) {
+        [self.toDoItems addObject:item];
+        [self.tableView reloadData];
+    }
 }
 
 - (void)viewDidLoad {
